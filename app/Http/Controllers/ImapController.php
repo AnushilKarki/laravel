6aa470ebfc5ed1cbdf8fd4c4fd4e15489190ea64
Anushil Karki <anushil.karki34@gmail.com>
2023-07-19 18:03:00 +0545

    for image save

--- app/Http/Controllers/ImapController.php
+++ app/Http/Controllers/ImapController.php
@@ -23,16 +23,69 @@ class ImapController extends Controller
         $info = imap_fetchstructure($mbox, $MC->Nmsgs);
 // dd($info);
         $mails = imap_search($mbox, 'ALL');
+     
         foreach ($mails as $email) {
+            $header = imap_headerinfo($mbox,$email);
+  
             // Get email header information
             $overview = imap_fetch_overview($mbox, $email, 0);
             // Get email body
             $message = imap_fetchbody($mbox, $email, '1');
             $message = str_replace("\r\n"," ",$message);
             $date = date("d F, Y", strtotime($overview[0]->date));
+            $data[$email]=[
+                "overview"=>$overview,
+                "message"=>$message,
+                "date"=>$date
+            ];
         }
+        // now check fetch structure 
+        foreach($mails as $email) {
+    //         $structure = imap_fetchstructure($mbox,$email);
+    //         global $htmlmsg,$plainmsg,$charset,$attachments;
+    //         $partno = 0;
+    //         $data = ($partno)?
+    //         imap_fetchbody($mbox,$email,$partno):  // multipart
+    //         imap_body($mbox,$email);
+        
+    //         if ($structure->encoding==4)
+    //     $data = quoted_printable_decode($data);
+    // elseif ($structure->encoding==3)
+    //     $data = base64_decode($data);
+           
+    //     $params = array();
+    //     if ($structure->parameters)
+    //         foreach ($structure->parameters as $x)
+    //             $params[strtolower($x->attribute)] = $x->value;
+    //     if ($structure->ifdparameters)
+    //         foreach ($structure->ifdparameters as $x)
+    //             $params[strtolower($x->attribute)] = $x->value;
+    
+    }
+    foreach ($mails as $emailId) {
+        $emailStructure = imap_fetchstructure($mbox, $emailId);
+    $saveFolderPath = '../public/';
+        if (isset($emailStructure->parts) && count($emailStructure->parts) > 0) {
+            for ($i = 0; $i < count($emailStructure->parts); $i++) {
+                $part = $emailStructure->parts[$i];
+    dd($part);
+                if (isset($part->disposition) && strtoupper($part->disposition) === 'ATTACHMENT') {
+                    $filename = $part->dparameters[0]->value;
+                    $attachmentData = imap_fetchbody($mbox, $emailId, $i + 1);
+  
+                    // Save the attachment to the public folder
+                    $savePath = $saveFolderPath . $filename;
+                    file_put_contents($savePath, $attachmentData);
+    
+                    echo "Attachment saved: $filename\n";
+                }
+            }
+        }
+    }
+    
+ 
         echo $message;
-        dd($message);
+        dd($data);
         imap_close($mbox);
         //imap close closes the stream of imap
     }
