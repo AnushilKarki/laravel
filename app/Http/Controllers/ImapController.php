9e99268b2d51b7a4f7dd77cb555325e0d76815f2
Anushil Karki <anushil.karki34@gmail.com>
2023-07-21 15:39:33 +0545

    decode fix

--- app/Http/Controllers/ImapController.php
+++ app/Http/Controllers/ImapController.php
@@ -11,6 +11,8 @@ class ImapController extends Controller
         $mbox = imap_open ("{imap.gmail.com:993/imap/ssl/novalidate-cert}INBOX", "taxplextest@gmail.com", "nniiabtvmmfpgyjr") or die("can't connect: " . imap_last_error());
         // imap_open setup the connection for imap 
         $MC = imap_check($mbox);
+        $h = imap_header($mbox);
+        dd($h);
         //imap checks gets date,driver,mailbox,no of msgs and no of recents
         $result = imap_fetch_overview($mbox,"1:{$MC->Nmsgs}",0);
         // imap fetch overview fetches all mailbox messages details
@@ -19,28 +21,172 @@ class ImapController extends Controller
             {$overview->subject}\n";
         }
         $value = imap_body($mbox, $MC->Nmsgs);
+        // dd($value);
+        // $value = imap_base64($value);
+        // dd($value);
         $value = imap_utf8($value);
         $info = imap_fetchstructure($mbox, $MC->Nmsgs);
+    
 // dd($info);
-        $mails = imap_search($mbox, 'ALL');
-     
+        // $mails = imap_search($mbox, 'ALL');
+        // 'SINCE "2023-7-21"' , 'ALL', 'BEFORE "2023-7-21"' , 'ON "2023-7-21"', 'FROM "anushil.karki34@gmail.com"',UNSEEN
+        // 'TO "anushil.karki34@gmail.com"'
+        $mails = imap_search($mbox,'ALL');
+   // set uid to total count if count incresed then fetch remaining email else donot fetch
+   // uid = 0 
+   $uid = imap_uid($mbox,2);
+   dd($uid);
         foreach ($mails as $email) {
+            //uid = uid + 1 
             $header = imap_headerinfo($mbox,$email);
-  
+            $uid = imap_uid($mbox,$email);
+       
+            // dd($header);
+            // $head = imap_fetchheader($mbox,$email);
+        
+        //    dd($head);
+
+        
             // Get email header information
-            $overview = imap_fetch_overview($mbox, $email, 0);
+           
+            $emailStructure = imap_fetchstructure($mbox, $email);
+            // dd($emailStructure);
+            $encoding = $emailStructure->encoding;
+
+    // Fetch the email body based on the encoding type
+    switch ($encoding) {
+        case 0: // 7bit
+            foreach ($emailStructure->parts as $partNum => $partData) {
+                if($partData->encoding==3){
+                    $body = imap_fetchbody($mbox, $email, 1.1);
+                }
+                elseif($partData->encoding==4){
+                    $body = imap_fetchbody($mbox, $email, 1);
+                }
+                else {
+                    $body = imap_fetchbody($mbox, $email, 1);
+                }
+            }
+            break;
+        //     $body = imap_fetchbody($mbox, $email, 1);
+        //    $body = quoted_printable_decode($body);
+        // case 1: // 8bit
+        // case 2: // Binary
+        //     $body = imap_fetchbody($mbox, $email, 1);
+        //     break;
+        // case 3: // Base64
+        //     $body = imap_fetchbody($mbox, $email, 1.1);
+        //     // $body = base64_decode($email_body);
+        //     break;
+        // case 4: // Quoted-Printable
+        //     $body = imap_fetchbody($mbox, $email, 1);
+        //     // $body = quoted_printable_decode($email_body);
+        //     break;
+        // case 5: // Other
+        //     $body = imap_fetchbody($mbox, $email, 1);
+        //     break;
+        default:
+            $body = imap_body($mbox, $email);
+            $body = quoted_printable_decode($body);
+            break;
+    }
+
+// dd($emailStructure);
+
+// if($emailStructure->type!=0){
+//     if ($emailStructure->encoding==3){
+//         $body = imap_fetchbody($mbox, $email,1.1);
+//         $body=base64_decode($body);
+//     }
+//         //decode if quoted printable
+//       elseif ($emailStructure->encoding==4){
+//             $body = imap_fetchbody($mbox, $email,1);
+//             $body=quoted_printable_decode($body);
+//         }
+//         else {
+//             $body = imap_fetchbody($mbox, $email,1.1);
+         
+//         }
+// }
+// if($emailStructure->type==0){
+//     dd(1);
+//     //decode text
+//     //if QUOTED-PRINTABLE
+//     if ($emailStructure->encoding==3){
+//         $body = imap_fetchbody($mbox, $email,1.1);
+//         $body=base64_decode($body);
+//         dd($body);
+//     }
+//         //decode if quoted printable
+//       elseif ($emailStructure->encoding==4){
+//             $body = imap_fetchbody($mbox, $email,1);
+//             $body=quoted_printable_decode($body);
+//             dd($body);
+//         }
+//         else {
+//             $body = imap_fetchbody($mbox, $email,1);
+//             dd($body);
+         
+//         }
+// }
+// dd($body);
+            // dd($emailStructure->type); type 0 = plaIN TYPE=1 = MULTIPART
+            //quoted printable = 4 
+            // if ($emailStructure->encoding === 4) {
+            //     // Decode Quoted Printable
+            //     $body = imap_fetchbody($mbox, $email,1);
+            //     // $message = quoted_printable_decode($body);
+            // } 
+            // elseif ($emailStructure->encoding === 0) {
+            //     // 7bit = 0
+            //     $body = imap_fetchbody($mbox, $email,1.1);
+            //     $message = $body;
+            //     // $message = quoted_printable_decode($body);
+            // }
+            // elseif ($emailStructure->encoding === 1) {
+            //     // 8bit = 1
+            //     $body = imap_fetchbody($mbox, $email,1);
+            //     // $message = imap_8bit($body);
+            // }
+            // elseif ($emailStructure->encoding === 3) {
+            //     // Decode Base64 = 3
+            //     $body = imap_fetchbody($mbox, $email,1.1);
+            //     $message = $body;
+            //     // $message = base64_decode($body);
+            // } else {
+            //     // Assume plain text if no special encoding
+            //     $body = imap_fetchbody($mbox, $email,1);
+            //     $message = $body;
+            // }
+        //     if($emailStructure->type==0){
+        //          $message = imap_fetchbody($mbox, $email, '1');
+        //     }
+        //     if($emailStructure->type==1){
+        //         $message = imap_fetchbody($mbox, $email, '1.1');
+        //    }
+            // else
+            // {
+            //     $message = imap_fetchbody($mbox, $email, '1.1');
+            // }
             // Get email body
-            $message = imap_fetchbody($mbox, $email, '1');
-            $message = str_replace("\r\n"," ",$message);
-            $date = date("d F, Y", strtotime($overview[0]->date));
+            // $message = imap_fetchbody($mbox, $email, '1');
+            //fetch body with set 1 works for plain text and 1.1 for attachment messages
+            // $message = imap_fetchbody($mbox, $email, '1.1');
+            $body = str_replace("\r\n"," ",$body);
+            $body = strip_tags($body);
+          
+            $date = date("d F, Y", strtotime($result[0]->date));
             $data[$email]=[
-                "overview"=>$overview,
-                "message"=>$message,
-                "date"=>$date
+                "overview"=>$result,
+                "message"=>$body,
+                "date"=>$date,
+                "type"=>$emailStructure->type,
+                "encoding"=>$emailStructure->encoding
             ];
         }
+        dd($data);
         // now check fetch structure 
-        foreach($mails as $email) {
+        // foreach($mails as $email) {
     //         $structure = imap_fetchstructure($mbox,$email);
     //         global $htmlmsg,$plainmsg,$charset,$attachments;
     //         $partno = 0;
@@ -61,7 +207,8 @@ class ImapController extends Controller
     //         foreach ($structure->ifdparameters as $x)
     //             $params[strtolower($x->attribute)] = $x->value;
     
-    }
+    // }
+
     // foreach ($mails as $emailId) {
     //     $emailStructure = imap_fetchstructure($mbox, $emailId);
     //     dd($emailStructure);
@@ -82,6 +229,7 @@ class ImapController extends Controller
     //         }
     //     }
     // }
+
     // foreach ($mails as $emailId) {
     //     // Fetch the email structure
     //     $emailStructure = imap_fetchstructure($mbox, $emailId);
@@ -108,47 +256,48 @@ class ImapController extends Controller
     //         }
     //     }
     // }
-     $attachments = array();
+
+    //  $attachments = array();
     foreach ($mails as $emailId) {
         // Fetch the email structure
         $emailStructure = imap_fetchstructure($mbox, $emailId);
 
         // Loop through the email parts to find attachments
         foreach ($emailStructure->parts as $partNum => $partData) {
-              $attachments[$partNum] = array(
-                'is_attachment' => false,
-                'filename' => '',
-                'name' => '',
-                'attachment' => ''
-            );
+            //   $attachments[$partNum] = array(
+            //     'is_attachment' => false,
+            //     'filename' => '',
+            //     'name' => '',
+            //     'attachment' => ''
+            // );
            
-            if($emailStructure->parts[$partNum]->ifdparameters) {
-                foreach($emailStructure->parts[$partNum]->dparameters as $object) {
-                    if(strtolower($object->attribute) == 'filename') {
-                        $attachments[$partNum]['is_attachment'] = true;
-                        $attachments[$partNum]['filename'] = $object->value;
-                    }
-                }
-            }
+            // if($emailStructure->parts[$partNum]->ifdparameters) {
+            //     foreach($emailStructure->parts[$partNum]->dparameters as $object) {
+            //         if(strtolower($object->attribute) == 'filename') {
+            //             $attachments[$partNum]['is_attachment'] = true;
+            //             $attachments[$partNum]['filename'] = $object->value;
+            //         }
+            //     }
+            // }
            
-            if($emailStructure->parts[$partNum]->ifparameters) {
-                foreach($emailStructure->parts[$partNum]->parameters as $object) {
-                    if(strtolower($object->attribute) == 'name') {
-                        $attachments[$partNum]['is_attachment'] = true;
-                        $attachments[$partNum]['name'] = $object->value;
-                    }
-                }
-            }
+            // if($emailStructure->parts[$partNum]->ifparameters) {
+            //     foreach($emailStructure->parts[$partNum]->parameters as $object) {
+            //         if(strtolower($object->attribute) == 'name') {
+            //             $attachments[$partNum]['is_attachment'] = true;
+            //             $attachments[$partNum]['name'] = $object->value;
+            //         }
+            //     }
+            // }
            
-            if($attachments[$partNum]['is_attachment']) {
-                $attachments[$partNum]['attachment'] = imap_fetchbody($mbox, $emailId, $partNum+1);
-                if($emailStructure->parts[$partNum]->encoding == 3) { // 3 = BASE64
-                    $attachments[$partNum]['attachment'] = base64_decode($attachments[$partNum]['attachment']);
-                }
-                elseif($emailStructure->parts[$partNum]->encoding == 4) { // 4 = QUOTED-PRINTABLE
-                    $attachments[$partNum]['attachment'] = quoted_printable_decode($attachments[$partNum]['attachment']);
-                }
-            }
+            // if($attachments[$partNum]['is_attachment']) {
+            //     $attachments[$partNum]['attachment'] = imap_fetchbody($mbox, $emailId, $partNum+1);
+            //     if($emailStructure->parts[$partNum]->encoding == 3) { // 3 = BASE64
+            //         $attachments[$partNum]['attachment'] = base64_decode($attachments[$partNum]['attachment']);
+            //     }
+            //     elseif($emailStructure->parts[$partNum]->encoding == 4) { // 4 = QUOTED-PRINTABLE
+            //         $attachments[$partNum]['attachment'] = quoted_printable_decode($attachments[$partNum]['attachment']);
+            //     }
+            // }
             // Check if it's an attachment
             if (isset($partData->disposition) && $partData->disposition === 'ATTACHMENT') {
                 // Generate a unique filename for each attachment
@@ -165,7 +314,7 @@ class ImapController extends Controller
                     }
               
                 $fileSaved = file_put_contents($filePath, $file);
-                dd($fileSaved);
+           
                 // Save the attachment to the file
                 // $attachmentSaved = imap_savebody($mbox, $filePath, $emailId, $partNum + 1);
                 // if ($attachmentSaved) {
